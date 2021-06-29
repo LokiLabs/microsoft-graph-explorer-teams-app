b4c17ed0c0016bf256a7195803a14a2a9dcfefa7
Grace Tsao <tsaosgrace@gmail.com>
2021-06-29 12:58:49 -0700

    Linked to the correct docs for graphs and also added a todo after MVP

--- ToolkitApp/tabs/src/components/sample/TabConstants.jsx
+++ ToolkitApp/tabs/src/components/sample/TabConstants.jsx
@@ -1,2 +1,3 @@
 export const GRAPH_EXPLORER_URL = "https://developer.microsoft.com/en-us/graph/graph-explorer";
-export const GRAPH_EXPLORER_DOCS_URL = "https://docs.microsoft.com/en-us/microsoftteams/platform/graph-api/rsc/resource-specific-consent#:~:text=Resource-specific%20consent%20%28RSC%29%20is%20a%20Microsoft%20Teams%20and,an%20application%20can%20do%20within%20a%20specific%20team%3A";
+export const GRAPH_EXPLORER_DOCS_URL = "https://docs.microsoft.com/en-us/graph/api/resources/teams-api-overview?view=graph-rest-1.0";
+//TO DO: Support multi languages
