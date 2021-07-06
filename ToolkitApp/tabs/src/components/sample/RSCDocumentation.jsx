af80d0bfecc741b3068ce2b6e96ece2f5fa62468
Grace Tsao <tsaosgrace@gmail.com>
2021-07-05 22:37:21 -0700

    Removed exclamation mark and filtered through the draw data

--- ToolkitApp/tabs/src/components/sample/RSCDocumentation.jsx
+++ ToolkitApp/tabs/src/components/sample/RSCDocumentation.jsx
@@ -12,7 +12,33 @@ export function RSCDocumentation() {
         const response = await fetch(RSC_DOCUMENATION_URL);
         const blob = await response.blob();
         var text = await blob.text();
-        var html = markdown(text);
+        var lines = text.split('\n');
+        var refinedText = ''
+        for(var i = 0;i < lines.length;i++){
+            // dead links in this section
+            if(lines[i] === '## Enable RSC in your application'){
+                i += 13;
+            }
+            // do not include the pictures 
+            if(!lines[i].includes('.png')){
+                console.log(i)
+                refinedText += lines[i];
+                if(i !== lines.length - 1){
+                    refinedText += '\n';
+                }
+            }
+            else{
+                // if there is a picture in the middle of a list
+                if(0 < i < lines.length - 1 && lines[i-1] === "" && lines[i+1] ===""){
+                    // remove the previous empty line
+                    refinedText = refinedText.slice(0, -1);
+                    // skip the next empty line
+                    i += 1
+                }
+            }
+
+        }
+        var html = markdown(refinedText);
         setRSCDocs(html);
     };
     return (
