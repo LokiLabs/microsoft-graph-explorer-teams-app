89b5167976dac9d0a315041b174a66c3efeccd77
Grace Tsao <tsaosgrace@gmail.com>
2021-07-21 23:47:25 -0700

    Writes the chat id

--- ToolkitApp/tabs/src/components/sample/ConnectedResources.jsx
+++ ToolkitApp/tabs/src/components/sample/ConnectedResources.jsx
@@ -1,66 +1,43 @@
-import React from 'react'
-import { List, Image } from '@fluentui/react-northstar'
+import React , { useState } from 'react';
+import { List, Image, Button } from '@fluentui/react-northstar'
+import * as microsoftTeams from "@microsoft/teams-js"; 
 
-class SelectableListControlledExample extends React.Component {
-  state = {
-    selectedIndex: -1,
-  }
-  items = [
-    {
-      key: 'robert',
-      media: (
-        <Image
-          src="https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobertTolbert.jpg"
-          avatar
-        />
-      ),
-      header: 'Robert Tolbert',
-      headerMedia: '7:26:56 AM',
-      content: 'Program the sensor to the SAS alarm through the haptic SQL card!',
-    },
-    {
-      key: 'celeste',
-      media: (
-        <Image
-          src="https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CelesteBurton.jpg"
-          avatar
-        />
-      ),
-      header: 'Celeste Burton',
-      headerMedia: '11:30:17 PM',
-      content: 'Use the online FTP application to input the multi-byte application!',
-    },
-    {
-      key: 'cecil',
-      media: (
-        <Image
-          src="https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/CecilFolk.jpg"
-          avatar
-        />
-      ),
-      header: 'Cecil Folk',
-      headerMedia: '5:22:40 PM',
-      content: 'The GB pixel is down, navigate the virtual interface!',
-    },
-  ]
-
-  render() {
-    return (
-      <List
-        selectable
-        selectedIndex={this.state.selectedIndex}
-        onSelectedIndexChange={(e, newProps) => {
-          alert(
-            `List is requested to change its selectedIndex state to "${newProps.selectedIndex}"`,
+export function processTeamsContext(setResourceList,resourceList ){
+    var obj = {}
+    microsoftTeams.getContext(function(context) {
+        console.log(context);
+        obj.endMedia = (
+            <Button content="Disconnect app" primary onClick={() =>console.log("clicked the button")}
+            />
           )
-          this.setState({
-            selectedIndex: newProps.selectedIndex,
-          })
-        }}
-        items={this.items}
-      />
-    )
-  }
+        obj.media = (
+            <Image
+              src="https://fabricweb.azureedge.net/fabric-website/assets/images/avatar/RobertTolbert.jpg"
+              avatar
+            />
+          )
+        obj.header = context.entityId;
+            obj.content = "Resource ID: " + context.chatId + " Resource Type:  Chat";
+        if(isAlreadyPresent(resourceList,obj.content)){
+            return; 
+        }
+        console.log("adding to the resourceList")
+        console.log(resourceList)
+        if(resourceList.length === 0){
+            setResourceList([obj])
+        }
+        else{
+            setResourceList(oldArray => [...oldArray, obj]);
+        }
+    });
+
 }
 
-export default SelectableListControlledExample
+function isAlreadyPresent(list, id){
+    for (let i = 0; i < list.length; i++){
+        if (id === list[i].content){
+            return true
+        }
+    }
+    return false
+}
--- ToolkitApp/tabs/src/components/sample/MainContent.jsx
+++ ToolkitApp/tabs/src/components/sample/MainContent.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { ChevronDownIcon, ChevronEndIcon, Header, Flex,List, Image } from '@fluentui/react-northstar';
-import ConnectedResources from './ConnectedResources.jsx';
+import { ChevronDownIcon, ChevronEndIcon, Header, Flex,List, Button, Image  } from '@fluentui/react-northstar';
+import { processTeamsContext } from './ConnectedResources.jsx';
 import { RSCList } from "./RSCList";
 import { RSCDocumentation } from "./RSCDocumentation";
 import { GRAPH_EXPLORER_URL, GRAPH_EXPLORER_DOCS_URL, OFFICIAL_RSC_URL } from "./TabConstants";
 import { QueryRunner } from './QueryRunner';
 import { useTranslation } from 'react-i18next';
 import DocumentationLinks from "./DocumentationLinks";
+
+import * as microsoftTeams from "@microsoft/teams-js"; 
 
 const dummyText =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vestibulum sed arcu non odio. Volutpat diam ut venenatis tellus in metus vulputate. Tellus in hac habitasse platea. Non quam lacus suspendisse faucibus interdum posuere. Est ante in nibh mauris cursus. Duis ut diam quam nulla porttitor massa. Eget felis eget nunc lobortis. Quisque non tellus orci ac auctor augue mauris augue neque. Nibh cras pulvinar mattis nunc sed blandit libero volutpat sed. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero id. Tristique nulla aliquet enim tortor at. Volutpat consequat mauris nunc congue. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Phasellus faucibus scelerisque eleifend donec pretium. Sed faucibus turpis in eu mi bibendum neque egestas. Purus in massa tempor nec feugiat nisl pretium fusce. Nunc sed velit dignissim sodales. Et netus et malesuada fames ac turpis egestas maecenas. Convallis tellus id interdum velit laoreet id donec ultrices tincidunt. Dui sapien eget mi proin sed libero enim sed faucibus. Tempus imperdiet nulla malesuada pellentesque. Aliquet eget sit amet tellus cras adipiscing enim. Cursus metus aliquam eleifend mi in. Elementum sagittis vitae et leo duis. Ac turpis egestas sed tempus urna. Ac odio tempor orci dapibus ultrices in iaculis nunc. Habitasse platea dictumst quisque sagittis purus sit. Sed velit dignissim sodales ut eu sem integer vitae justo.";
@@ -17,9 +19,11 @@ const MainContent = () => {
     const [thirdSectionActive, toggleThirdSection] = useState(true);
     const [fourthSectionActive, toggleFourthSection] = useState(true);
     const { t } = useTranslation();
+    const [connectedResourcesList, setResourceList] = useState([]);
 
     let firstSection;
     if (firstSectionActive) {
+        processTeamsContext(setResourceList, connectedResourcesList);   
         firstSection =
             <div>
                 <Flex id="main-section" gap="gap.small" onClick={() => toggleFirstSection(!firstSectionActive)}>
@@ -27,8 +31,7 @@ const MainContent = () => {
                     <Header id="connected-resource-header" as="h2" content={t("Table of Contents.Connected Resources")} />
                 </Flex>
                 <div>
-                  <ConnectedResources/>
-                  <p>{dummyText}</p>
+                    <List selectable defaultSelectedIndex={0}  items={connectedResourcesList}/> 
                 </div>
             </div>
     } else {
