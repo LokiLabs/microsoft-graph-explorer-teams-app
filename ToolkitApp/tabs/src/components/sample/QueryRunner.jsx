9acf01680975eaae45c7e180cd0ccb17d072d144
Lucy Hao <hao.lucyy@gmail.com>
2021-07-22 14:25:31 -0700

    changed to white for inputs and better request headers

--- ToolkitApp/tabs/src/components/sample/QueryRunner.jsx
+++ ToolkitApp/tabs/src/components/sample/QueryRunner.jsx
@@ -1,11 +1,61 @@
 import React, { useState, useEffect } from "react";
 import { requestTypes, graphVersions, GRAPH_URL } from "./TabConstants";
-import { FormInput, Button, FormDropdown, Flex, Menu, TextArea, Table, tabListBehavior, Dropdown } from '@fluentui/react-northstar'
-
+import { Button, Input, Flex, Menu, TextArea, Table, tabListBehavior, Dropdown } from '@fluentui/react-northstar'
+import { gridCellWithFocusableElementBehavior, } from '@fluentui/accessibility'
+import { TrashCanIcon, AddIcon } from '@fluentui/react-icons-northstar'
 import enUS from './GE.json';
-import { Alert } from '@fluentui/react-northstar'
 
 export function QueryRunner() {
+    const addRequestHeader = () => {
+        if (requestHeaders.filter(requestH => requestH.key === userAddedHeader).length === 0) {
+            requestHeaders.splice(requestHeaders.length - 1, 0, {
+                key: userAddedHeader,
+                items: [userAddedHeader, userAddedValue, {
+                    content: <Button
+                        tabIndex={-1}
+                        icon={<TrashCanIcon />}
+                        circular
+                        text
+                        iconOnly
+                        title="Delete request header"
+                        onClick={() => {
+                            setRequestHeaders(requestHeaders.filter(requestH => requestH.key !== userAddedHeader))
+                        }}
+                    />,
+                    truncateContent: true,
+                    accessibility: gridCellWithFocusableElementBehavior,
+                    onClick: e => {
+                        e.stopPropagation()
+                    },
+                }],
+            });
+            setRequestHeaders(requestHeaders);
+        }
+        setUserAddedHeader("");
+        setUserAddedValue("");
+    }
+
+    const addOptionCell = {
+        content: <Button tabIndex={-1} icon={<AddIcon />} circular text iconOnly title="Delete request header" />,
+        truncateContent: true,
+        accessibility: gridCellWithFocusableElementBehavior,
+        onClick: e => {
+            addRequestHeader()
+            e.stopPropagation()
+        },
+    }
+
+    const [userAddedHeader, setUserAddedHeader] = useState("");
+    const [userAddedValue, setUserAddedValue] = useState("");
+
+    const keyInputCell = {
+        content: <Input fluid={true} inverted={true} id="key" value={userAddedHeader} showSuccessIndicator={false} required onChange={(evt) => setUserAddedHeader(evt.target.value)} type="text" />,
+    }
+
+    const valueInputCell = {
+        content: <Input fluid={true} inverted={true} id="value" value={userAddedValue} showSuccessIndicator={false} required onChange={(evt) => setUserAddedValue(evt.target.value)} type="text" />,
+    }
+
     const [requestType, setRequestType] = useState(requestTypes[0]);
     const [graphVersion, setGraphVersion] = useState(graphVersions[0]);
     const [query, setQuery] = useState(GRAPH_URL);
@@ -21,9 +71,9 @@ export function QueryRunner() {
     const [requestComponentIndex, setRequestComponentIndex] = useState(0);
     const [requestHeaders, setRequestHeaders] = useState([
         {
-            key: 'Content',
-            items: ['Content', 'text'],
-        },
+            key: 'addNew',
+            items: [keyInputCell, valueInputCell, addOptionCell],
+        }
     ]);
 
     const requestItems = [
@@ -36,7 +86,11 @@ export function QueryRunner() {
         enUS["response headers"]
     ]
 
-    const header = {
+    const requestHeadersTable = {
+        items: ['Key', 'Value', ''],
+    }
+
+    const responseHeadersTable = {
         items: ['Key', 'Value'],
     }
 
@@ -46,62 +100,23 @@ export function QueryRunner() {
         console.log(query);
     }
 
-    const addRequestHeader = () => {
-        const header = document.getElementById("key").value;
-        const value = document.getElementById("value").value;
-        requestHeaders.push({
-            key: header,
-            items: [header, value],
-        });
-        setRequestHeaders(requestHeaders);
-    }
-
     useEffect(() => {
         setQuery(GRAPH_URL + graphVersion + query.substring(GRAPH_URL.length + 4, query.length));
     }, [graphVersion, query])
 
-    const requestComponents = [
-        <TextArea fluid={true} resize="both" value={requestBody} onChange={(evt) => setRequestBody(evt.target.value)} />,
-        <>
-            <Flex gap="gap.small" padding="padding.medium">
-                <Flex.Item size="size.half">
-                    <FormInput
-                        name="key"
-                        id="key"
-                        fluid={true}
-                    />
-                </Flex.Item>
 
-                <Flex.Item size="size.half">
-                    <FormInput
-                        name="value"
-                        id="value"
-                        fluid={true}
-                    />
-                </Flex.Item>
-                <Button content={enUS["add"]} onClick={() => addRequestHeader()} primary />
-            </Flex>
-            <Table compact header={header} rows={requestHeaders} aria-label="request headers" />
-        </>
+    const requestComponents = [
+        <TextArea fluid={true} inverted={true} resize="both" value={requestBody} onChange={(evt) => setRequestBody(evt.target.value)} />,
+        <Table compact header={requestHeadersTable} rows={requestHeaders} aria-label="request headers" />
     ]
 
     const responseComponents = [
-        <TextArea fluid={true} resize="both" value={responseBody} />,
-        <Table compact header={header} rows={responseHeaders} aria-label="respons headers" />
+        <TextArea fluid={true} inverted={true} resize="both" value={responseBody} />,
+        <Table compact header={responseHeadersTable} rows={responseHeaders} aria-label="response headers" />
     ]
 
     return (
-
         <>
-            {/* <Alert
-                style={{ "margin-top": 15 + 'px' }}
-                warning
-                content={enUS["warning"]}
-                dismissible
-                dismissAction={{
-                    'aria-label': 'close',
-                }}
-            /> */}
             <h2>{enUS["query runner"]}</h2>
 
             <Flex gap="gap.small">
@@ -109,6 +124,7 @@ export function QueryRunner() {
                     <Flex gap="gap.small">
                         <Flex.Item size="size.half">
                             <Dropdown
+                                inverted={true}
                                 fluid={true}
                                 items={requestTypes}
                                 id="requestType"
@@ -118,6 +134,7 @@ export function QueryRunner() {
                         </Flex.Item>
                         <Flex.Item size="size.half">
                             <Dropdown
+                                inverted={true}
                                 fluid={true}
                                 items={graphVersions}
                                 id="graphVersion"
@@ -129,7 +146,7 @@ export function QueryRunner() {
                 </Flex.Item>
 
                 <Flex.Item grow>
-                    <FormInput fluid={true} value={query} required onChange={(evt) => setQuery(evt.target.value)} type="text" />
+                    <Input fluid inverted value={query} showSuccessIndicator={false} required onChange={(evt) => setQuery(evt.target.value)} type="text" />
                 </Flex.Item>
 
                 <Button content={enUS["run query"]} onClick={() => callGraph()} primary />
