905385ceec5df828525657bc254d1c827022311a
Jerry He <jerryxhe@gmail.com>
2021-07-26 11:42:07 -0600

    Added localization support for documentation link buttons

--- ToolkitApp/tabs/src/components/sample/DocumentationLinks.jsx
+++ ToolkitApp/tabs/src/components/sample/DocumentationLinks.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { GRAPH_EXPLORER_URL, GRAPH_EXPLORER_DOCS_URL, OFFICIAL_RSC_URL} from "./TabConstants";
 import { Button, Flex } from '@fluentui/react-northstar';
+import { useTranslation } from 'react-i18next';
 
 const DocumentationLinks = () => {
+    const { t } = useTranslation();
+
     return (
         <Flex gap="gap.large">
-            <Button content="Go to Graph Explorer" onClick={() => window.open(GRAPH_EXPLORER_URL)} primary />
-            <Button content="Graph Explorer Documentation" onClick={() => window.open(GRAPH_EXPLORER_DOCS_URL)} primary />
-            <Button content="Resource-Specific Consent" onClick={() => window.open(OFFICIAL_RSC_URL)} primary />
+            <Button content={t("Documentation Links.Go to Graph Explorer")} onClick={() => window.open(GRAPH_EXPLORER_URL)} primary />
+            <Button content={t("Documentation Links.Graph Explorer Documentation")} onClick={() => window.open(GRAPH_EXPLORER_DOCS_URL)} primary />
+            <Button content={t("Documentation Links.Resource-Specific Consent")} onClick={() => window.open(OFFICIAL_RSC_URL)} primary />
         </Flex>
     );
 };
