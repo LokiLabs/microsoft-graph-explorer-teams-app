6d2fb6426e6bdf4a6305b243d77d9d443214b417
Lucy Hao <hao.lucyy@gmail.com>
2021-08-02 10:29:42 -0700

    return

--- tabs/src/components/sample/ConnectedResources.jsx
+++ tabs/src/components/sample/ConnectedResources.jsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Button, List, Alert, ListItem } from '@fluentui/react-northstar';
 import { ClipboardCopiedToIcon } from '@fluentui/react-icons-northstar';
-import copy from "copy-to-clipboard"; 
-import * as microsoftTeams from "@microsoft/teams-js"; 
-import {TEAMS_CHANNEL_ID, CHAT, CHAT_ID,  NO_CONNECTED_RESOURCES} from './TabConstants';
+import copy from "copy-to-clipboard";
+import * as microsoftTeams from "@microsoft/teams-js";
+import { TEAMS_CHANNEL_ID, CHAT, CHAT_ID, NO_CONNECTED_RESOURCES } from './TabConstants';
 
-export function ProcessTeamsContext(){
+export function ProcessTeamsContext() {
     //Get the context of where the tab is currently
     const [resourceList, setResourceList] = useState([]);
     const [title, setTitle] = useState(" ");
-    microsoftTeams.getContext(function(context) {
-        
+    microsoftTeams.getContext(function (context) {
+
         //Check if this is a teams channel
-        if(context.channelId && context.channelId.length !== 0){
-            if(isAlreadyPresent(resourceList, context.channelId)){
-                return; 
+        if (context.channelId && context.channelId.length !== 0) {
+            if (isAlreadyPresent(resourceList, context.channelId)) {
+                return;
             }
             var channelId = createItemWithCopy(context.channelId);
             channelId.header = TEAMS_CHANNEL_ID + context.channelId;
@@ -22,46 +22,54 @@ export function ProcessTeamsContext(){
             setResourceList([channelId]);
         }
         //Check if it is a chat
-        else if(context.chatId && context.chatId.length !== 0){
-            if(isAlreadyPresent(resourceList, context.chatId)){
-                return; 
+        else if (context.chatId && context.chatId.length !== 0) {
+            if (isAlreadyPresent(resourceList, context.chatId)) {
+                return;
             }
             setTitle(CHAT);
             var chatId = createItemWithCopy(context.chatId);
             chatId.header = CHAT_ID + context.chatId;
             setResourceList([chatId]);
         }
-        else{
+        else {
             setTitle("");
             setResourceList([]);
         }
     });
     return (
         <div className="connected-resource">
-            {resourceList.length !== 0 && <ListItem className = "title" header = {title}/>}
-            {resourceList.length !== 0 && <List selectable defaultSelectedIndex={0} items={resourceList}/>} 
-            {resourceList.length === 0 && !title &&  <Alert danger content={NO_CONNECTED_RESOURCES}/>}
+            {resourceList.length !== 0 && <ListItem className="title" header={title} />}
+            {resourceList.length !== 0 && <List selectable defaultSelectedIndex={0} items={resourceList} />}
+            {resourceList.length === 0 && !title && <Alert danger content={NO_CONNECTED_RESOURCES} />}
         </div>
     );
 }
 
-function isAlreadyPresent(list, id){
+function isAlreadyPresent(list, id) {
     //if there aren't any contexts
-    list.some((item => {
+    return list.some((item => {
         item.header?.includes(id);
     }));
 }
 
-function copyText(id){
+function copyText(id) {
     copy(id);
 }
 
-export function createItemWithCopy(id){
+export function createItemWithCopy(id) {
     var item = {};
 
     //Add the copy icon 
     item.endMedia = (
-        <Button aria-label="copy" icon={<ClipboardCopiedToIcon  className = "button-icon"  />} size="medium"  text iconOnly title="Copy" onClick={() => copyText(id)}/>
+        <Button
+            aria-label="copy"
+            icon={<ClipboardCopiedToIcon className="button-icon" />}
+            size="medium"
+            text
+            iconOnly
+            title="Copy"
+            onClick={() => copyText(id)}
+        />
     );
     return item;
 }
