b1370b88a5237f8dc6fd6b6ec99e47f351a79786
Lucy Hao <hao.lucyy@gmail.com>
2021-08-09 10:30:21 -0700

    move out of sample folder and rename to components

--- tabs/src/index.jsx
+++ tabs/src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 import "./index.css";
 
-import './components/i18n';
+import './i18n';
 
 ReactDOM.render(
   <App />,
