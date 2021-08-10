a7ca3140793c05a39499c585bd625f2ef96bed39
Jerry He <jerryxhe@gmail.com>
2021-08-10 10:16:37 -0600

    Changed documentation buttons to hyperlinks

--- tabs/src/components/app/views/DocumentationLinks.jsx
+++ tabs/src/components/app/views/DocumentationLinks.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import { UNLOCALIZED_GRAPH_EXPLORER_URL, UNLOCALIZED_GRAPH_EXPLORER_DOCS_URL, UNLOCALIZED_OFFICIAL_RSC_URL, localizeUrl } from "../../TabConstants";
-import { Button } from '@fluentui/react-northstar';
 import { useTranslation } from 'react-i18next';
+import { useTeamsFx } from "../../utils/useTeamsFx";
 
 export const DocumentationLinks = () => {
     const { t, i18n } = useTranslation();
+    const { themeString } = useTeamsFx();
+
+    let linkColor = "#6264A7";
+    switch(themeString) {
+        case "default":
+            linkColor = "#6264A7";
+            break;
+        case "dark":
+            linkColor = "#A6A7DC";
+            break;
+        case "contrast":
+            linkColor = "#ffff01";
+            break;
+        default:
+            linkColor = "#6264A7";
+    }
 
     return (
         <div className="documentation-links">
-            <Button
-                content={t("Documentation Links.Go to Graph Explorer")}
-                onClick={() => window.open(localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_URL, i18n))}
-                primary
-            />
-            <Button
-                content={t("Documentation Links.Graph Explorer Documentation")}
-                onClick={() => window.open(localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_DOCS_URL, i18n))}
-                primary
-            />
-            <Button
-                content={t("Documentation Links.Resource-Specific Consent")}
-                onClick={() => window.open(localizeUrl(UNLOCALIZED_OFFICIAL_RSC_URL, i18n))}
-                primary
-            />
+            <ul>
+                <li>
+                    <a className="documentation-link" href={localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_URL, i18n)} target="_blank" rel="noreferrer noopener" style={{ color: linkColor }}>
+                        {t("Documentation Links.Go to Graph Explorer")}
+                    </a>
+                </li>
+                <li>
+                    <a className="documentation-link" href={localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_DOCS_URL, i18n)} target="_blank" rel="noreferrer noopener" style={{ color: linkColor }}>
+                        {t("Documentation Links.Graph Explorer Documentation")}
+                    </a>
+                </li>
+                <li>
+                    <a className="documentation-link" href={localizeUrl(UNLOCALIZED_OFFICIAL_RSC_URL, i18n)} target="_blank" rel="noreferrer noopener" style={{ color: linkColor }}>
+                        {t("Documentation Links.Resource-Specific Consent")}
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
