114882381f95ea17277872d3207708424e29c5ea
Jerry He <jerryxhe@gmail.com>
2021-08-10 13:21:07 -0600

    Created MediumFontLink component and refactored DocumentationLinks

--- tabs/src/components/app/views/DocumentationLinks.jsx
+++ tabs/src/components/app/views/DocumentationLinks.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { UNLOCALIZED_GRAPH_EXPLORER_URL, UNLOCALIZED_GRAPH_EXPLORER_DOCS_URL, UNLOCALIZED_OFFICIAL_RSC_URL, localizeUrl } from "../../TabConstants";
 import { useTranslation } from 'react-i18next';
 import { useTeamsFx } from "../../utils/useTeamsFx";
+import { MediumFontLink } from "./MediumFontLink";
 
 export const DocumentationLinks = () => {
     const { t, i18n } = useTranslation();
@@ -23,24 +24,16 @@ export const DocumentationLinks = () => {
     }
 
     return (
-        <div className="documentation-links">
-            <ul>
-                <li>
-                    <a className="documentation-link" href={localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_URL, i18n)} target="_blank" rel="noreferrer noopener" style={{ color: linkColor }}>
-                        {t("Documentation Links.Go to Graph Explorer")}
-                    </a>
-                </li>
-                <li>
-                    <a className="documentation-link" href={localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_DOCS_URL, i18n)} target="_blank" rel="noreferrer noopener" style={{ color: linkColor }}>
-                        {t("Documentation Links.Graph Explorer Documentation")}
-                    </a>
-                </li>
-                <li>
-                    <a className="documentation-link" href={localizeUrl(UNLOCALIZED_OFFICIAL_RSC_URL, i18n)} target="_blank" rel="noreferrer noopener" style={{ color: linkColor }}>
-                        {t("Documentation Links.Resource-Specific Consent")}
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <ul>
+            <li>
+                <MediumFontLink URL={localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_URL, i18n)} content={t("Documentation Links.Go to Graph Explorer")} linkColor={linkColor} />
+            </li>
+            <li>
+                <MediumFontLink URL={localizeUrl(UNLOCALIZED_GRAPH_EXPLORER_DOCS_URL, i18n)} content={t("Documentation Links.Graph Explorer Documentation")} linkColor={linkColor} />
+            </li>
+            <li>
+                <MediumFontLink URL={localizeUrl(UNLOCALIZED_OFFICIAL_RSC_URL, i18n)} content={t("Documentation Links.Resource-Specific Consent")} linkColor={linkColor} />
+            </li>
+        </ul>
     );
 };
